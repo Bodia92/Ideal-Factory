f163e578a2df36b7fd918dda5b3f6518cbb5eed6
Bohdan Ponamariov <bodiakpi1992@gmail.comm>
2022-09-17 00:09:53 +0300

    fix slider full screen

--- src/js/components/sliderFullscreen.js
+++ src/js/components/sliderFullscreen.js
@@ -1,6 +1,26 @@
 import 'ScssComponents/_slider_fullscreen.scss';
+import Swiper, { Scrollbar } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
 
 const sliderFullscreen = () => {
+	const sliderClass = '.slider_fullscreen__swiper';
+
+	let slideEl = document.querySelectorAll(sliderClass);
+
+	if (typeof (slideEl) !== 'undefined' && slideEl != null) {
+		let sliderEl = new Swiper(sliderClass, {
+			modules: [Scrollbar],
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+				dragSize: 'auto',
+			},
+			slidesPerView: 'auto',
+			speed: 800,
+			// loop: true,
+		});
+	}
 };
 
 export default sliderFullscreen;
