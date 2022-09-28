5c2649fa0719c2421aa86f9b7fd60cff135b4e45
Bohdan Ponamariov <bodiakpi1992@gmail.comm>
2022-09-28 17:57:19 +0300

    improvements(slider full screen) added responsive tablet and mobile

--- src/js/components/sliderFullscreen.js
+++ src/js/components/sliderFullscreen.js
@@ -11,27 +11,25 @@ const sliderFullscreen = () => {
 	if (typeof (slideEl) !== 'undefined' && slideEl != null) {
 		let sliderEl = new Swiper(sliderClass, {
 			modules: [Scrollbar],
-			scrollbar: {
-				el: '.slider_fullscreen__scrollbar',
-				draggable: true,
-				dragSize: 'auto',
-				dragClass: 'slider_fullscreen__drag',
-			},
+
 			slidesPerView: 'auto',
 			speed: 1200,
 			// loop: true,
 
 			breakpoints: {
 				320: {
-					slidesPerView: 1.3,
+					slidesPerView: 1.2,
 				},
-				640: {
-					slidesPerView: 2.1,
+				768: {
+					slidesPerView: 1.78,
 				},
 				992: {
-					slidesPerView: 2.5,
-				},
-				1023: {
+					scrollbar: {
+						el: '.slider_fullscreen__scrollbar',
+						draggable: true,
+						dragSize: 'auto',
+						dragClass: 'slider_fullscreen__drag',
+					},
 					slidesPerView: 'auto',
 				},
 			},
